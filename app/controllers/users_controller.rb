7af594e68848501e406bdd7fa7e01a82a3b80feb
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-11-30 23:24:23 +0200

    Create the user model

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      cookies[:current_user_id] = @user.id
+      redirect_to root_path, notice: 'Successfully signed up!'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(session[:current_user_id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+end
