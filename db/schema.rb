df5df8daa34c068d113ce3369d7d69513bc80e4c
alejandrotoledoweb <atoledofr@gmail.com>
2020-12-01 15:14:39 -0500

    add the foreign keys for event_attendance

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_163732) do
+ActiveRecord::Schema.define(version: 2020_12_01_200907) do
+
+  create_table "event_attendances", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.integer "event_id", null: false
+    t.index ["event_id"], name: "index_event_attendances_on_event_id"
+    t.index ["user_id"], name: "index_event_attendances_on_user_id"
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -28,4 +37,6 @@ ActiveRecord::Schema.define(version: 2020_12_01_163732) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "event_attendances", "events"
+  add_foreign_key "event_attendances", "users"
 end
