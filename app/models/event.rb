a1e42f8570181e2141783224d4a04698e108aff3
alejandrotoledoweb <atoledofr@gmail.com>
2020-12-01 16:44:04 -0500

    add has_many event_attendances for User and Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
-  has_many :attendees, through: :event_attendance, source: :attendee
+  has_many :attendees, through: :event_attendances, source: :attendee
+  has_many :event_attendances, foreign_key: :attended_event_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true
   has_many :events, foreign_key: 'creator_id', class_name: 'Event'
-  has_many :attended_events, through: :event_attendance, source: :attended_event
+  has_many :attended_events, through: :event_attendances, source: :attended_event
+  has_many :event_attendances, foreign_key: :attendee_id
 end
