7e13c7e4716c1821f5b0411affa1445d840bda22
alejandrotoledoweb <atoledofr@gmail.com>
2020-12-02 20:56:32 -0500

    add test for models with Rspec

---
+++ spec/models/attendance_spec.rb
@@ -0,0 +1,14 @@
+
+require 'rails_helper'
+RSpec.describe Attendance, type: :model do
+  user = User.create(name: 'User1')
+  Event.create(title: 'Event test', description: 'Description test', venue: 'Location 1', date: '2020-10-22', creator_id: user.id)
+  user = User.first
+  event = Event.first
+  test_array = user.attended_events << event
+  it 'it should be equal to event title' do
+    test_array.each do |attendee|
+      expect(attendee.title).to eq('Event test')
+    end
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  user = User.new
+
+  it 'is valid when the name is present' do
+    user.name = 'User1'
+    expect(user).to be_valid
+  end
+
+  it 'is valid when name is present' do
+    user.name = nil
+    expect(user).to_not be_valid
+  end
+
+end
