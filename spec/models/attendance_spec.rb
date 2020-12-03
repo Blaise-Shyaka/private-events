6b884d5840986fd591c07f329f52869cd79fbb9d
alejandrotoledoweb <atoledofr@gmail.com>
2020-12-02 21:28:33 -0500

    fix linter issues

--- spec/models/attendance_spec.rb
+++ spec/models/attendance_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 RSpec.describe Attendance, type: :model do
   user = User.create(name: 'User1')
-  Event.create(title: 'Event test', description: 'Description test', 
-    venue: 'Location 1', date: '2020-10-22', creator_id: user.id)
+  Event.create(title: 'Event test', description: 'Description test',
+              venue: 'Location 1', date: '2020-10-22', creator_id: user.id)
   user = User.first
   event = Event.first
   test_array = user.attended_events << event
